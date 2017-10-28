8dfbbb2b88b2a26d18f4387be92cad84487c19c3
SamuelWarner <samuel.vfx@gmail.com>
2017-10-27 19:46:43 -0600

    Starter scripts for the project

---
+++ matrix_operations.py
@@ -0,0 +1,177 @@
+"""
+Basic matrix operations package. All functions assume matrix format of a list of lists. Formally created as "xmath"
+module for DOONN project.
+
+Version 1.0
+2017.10.15
+"""
+
+
+def add_2d(a, b):
+    """
+    Adds matrix 'a' with matrix 'b' to produce a matrix result
+    that contains the added values.
+
+    :param a:   2d matrix of type 'list' to be added to 'b'
+    :param b:   2d matrix of type 'list' to be added with 'a'
+    :return:    (list) 2d matrix result of a + b
+    """
+
+    # check if given matrices are of "list" type
+    if type(a) != list or type(b) != list:
+        raise TypeError('Error xm02:Incorrect type, matrices must be of type "list"')
+
+    # check if given matrices are of matching row size
+    if len(a) != len(b):
+        raise IndexError("Error xm03: Mismatch in matrix row count")
+
+    # compare matrix columns, all rows must have the same number of columns
+    i = 0
+    columns = len(a[i])
+
+    while i < len(a):
+        if len(a[i]) != columns or len(b[i]) != columns:
+            raise Exception("Error xm04: Incorrect matrix dimensions in row: %s" % i)
+        i += 1
+
+    # add matrices and return matrix result
+    zipped = list(zip(a, b))
+    return [[sum(column) for column in zip(*group)] for group in zipped]
+
+
+def multiply_2d(a, b):
+    """
+    Multiply two matrices (a*b) using zip functions. Matrices must have
+    dimensions that allow multiplication (ie. M x N and N x L).
+
+    :param a:  (list) 2D matrix
+    :param b:  (list) 2D matrix
+    :return:   (list) 2D matrix containing the result of a*b
+    """
+
+    # check if given matrices are of "list" type
+    if type(a) != list or type(b) != list:
+        raise TypeError('Error xm05: Incorrect type, matrices must be of type "list"')
+
+    # check that row length is consistent in matrix a
+    c = 0
+    while c < len(a):
+        if len(a[c]) != len(a[0]):
+            raise ArithmeticError("Error xm06: Incorrect column dimensions in matrix a")
+        c += 1
+
+    # check that row length is consistent in matrix b
+    c = 0
+    while c < len(b):
+        if len(b[c]) != len(b[0]):
+            raise ArithmeticError("Error xm07: Incorrect column dimensions in matrix b")
+        c += 1
+
+    # check if dimensions allow for multiplication
+    if len(a[0]) != len(b):
+        raise ArithmeticError("Error xm08: Can not multiply a %sx%s matrix by a %sx%s matrix"
+                        % (len(a), len(a[0]), len(b), len(b[0])))
+
+    # Multiply Matrices and return resulting matrix
+    zipped = zip(*b)
+    zipped = list(zipped)
+    return [[sum(ele_a * ele_b for ele_a, ele_b in zip(row_a, col_b))
+             for col_b in zipped] for row_a in a]
+
+
+def subtract_2d(a, b):
+    """
+    Function subtracts two matrices and returns a matrix result.
+
+    :param a:    (list) 2D matrix
+    :param b:    (list) 2D matrix
+    :return:     (list) 2D matrix containing the result of a-b
+    """
+
+    # zip a and inverted b then add the two matrix together
+    zipped = list(zip(a, [[b[r][i] * -1 for i in range(len(b[r]))] for r in range(len(b))]))
+    return [[sum(column) for column in zip(*group)] for group in zipped]
+
+
+def transpose_2d(a):
+    """
+    Transpose a given matrix using Zip. A 1x4 matrix becomes a 4x1 matrix
+
+    :param a: (list) 2D Matrix to transpose
+    :return:  (list) Transposed 2d matrix of a
+    """
+
+    # check if given matrix is list
+    if type(a) != list:
+        raise TypeError('Error xm10: Incorrect type, matrices must be of type "list"')
+
+    # check that rows are of matching length
+    l = len(a[0])
+    for row in a:
+        if len(row) != l:
+            raise Exception("Error xm11: Row lengths do not match")
+
+    # return transposed matrix
+    return list(map(list, zip(*a)))
+
+
+def multiply_constant(c, a):
+    """
+    Multiplies each element in matrix 'a' by a constant value 'c'.
+
+    :param c:   (int/float) Value to multiply elements in a with.
+    :param a:   (list) 2D matrix
+    :return:    (list) 2D matrix result of c*a.
+    """
+
+    # check argument types
+    if (type(c) != float and type(c) != int) or type(a) != list:
+        raise TypeError('Error xm13: Incorrect argument type')
+
+    # Return a matrix multiplied by the constant
+    return [[a[r][i] * c for i in range(len(a[r]))] for r in range(len(a))]
+
+
+def multiply_basic(a, b):
+    """
+    This function is NOT a standard matrix multiplication operation. It
+    instead multiplies two matrix directly(first index with first index).
+    Both Matrices must be: MxN where M=1 and N=any positive number.
+
+    :param a:  (list) 2D matrix with only one row
+    :param b:  (list) 2D matrix with only one row
+    :return:   (list) 2D matrix containing the product of a*b
+    """
+
+    # Check if both matrix contain only one row.
+    if len(a) != 1 or len(b) != 1:
+        raise Exception("Error xm14: Basic multiplication only works on 1xN matrices")
+    # Check for mismatched row lenght
+    if len(a[0]) != len(b[0]):
+        raise Exception("Error xm15: Row lengths do not match")
+
+    # multiply elements together and return matrix
+    return [[b[0][i] * a[0][i] for i in range(len(a[0]))]]
+
+
+def sum_2d(a):
+    """
+    Sum all the values in a matrix together.
+
+    :param a:  (list) 2D matrix containing values to be summed
+    :return:   (int/float) sum value of the matrix.
+    """
+
+    # Check if given matrices are of "list" type
+    if type(a) != list:
+        raise TypeError('Error xm01: Incorrect type, matrices must be of type "list"')
+
+    # add each value in each row to a total sum
+    total_sum = 0
+    for row in a:
+        for column in row:
+            total_sum += column
+
+    # return the sum
+    return total_sum
+
---
+++ sigma_network.py
@@ -0,0 +1,256 @@
+"""
+Contains single hidden layer Neural Network class designed with the sigma activation/threshold function.
+
+Dependencies:
+-matrix_math
+
+Version 1.0
+2017.10.15
+"""
+from matrix_operations import *
+import math
+import random
+import copy
+
+
+class Sigmoid:
+    def __init__(self):
+        pass
+
+    def activate(self, x):
+        """
+        Returns the Sigmoid of "x"
+
+        :param x: (int/float)  Value of x in the sigmoid function
+        :return:  (float)      Y value of x
+        """
+        return 1 / (1 + math.exp(-x))
+
+        # Possibly more stable implementation of sigmoid
+        # if x < 0:
+        #     return 1 - 1 / (1 + math.exp(-x))
+        # else:
+        #     return 1 / (1 + math.exp(-x))
+
+    def derivative(self, x):
+        """
+        Returns the sigmoid derivative of "x".
+
+        :param x: (int/float) Value to find the derivative of
+        :return:  (float)     Sigmoid derivative of x
+        """
+        return (math.exp(-x)) / (1 + math.exp(-x)) ** 2
+
+
+class Relu:
+    def __init__(self):
+        self.a = 0.01
+
+    def activate(self, x):
+        return max(self.a*x, x)
+
+    def derivative(self, x):
+        if x < 0:
+            return self.a
+        elif x >= 0:
+            return 1.0
+
+
+class Maze:
+    def __init__(self, network_layers, activation_function):
+        # Check arguments for bad input
+        if type(network_layers) != list:
+            raise TypeError("SigNet.__init__(self, network_layers) expected list for network_layers, "
+                            "got %s" % type(network_layers))
+        elif len(network_layers) < 2:
+            raise IndexError("network_layers incorrect length; network must include at least 2 layers")
+
+        # Initialize class variables
+        self.layers = len(network_layers)    # number of layers in the network
+        self.layout = network_layers         # network layout as a list
+        self.weights = []                    # all network weights
+        self.layer_sums = []                 # input sums for all layers in network(excluding input layer)
+        self.layer_inputs = []               # stores each layers "input" values during forward prop
+        self.__bias = True                   # determines if network should include a bias node on every layer
+        self.learning_constant = 0.1         # learning constant, higher value = more aggressive weight adjustments
+
+
+        if activation_function == 'sig': self.act = Sigmoid() # activation function
+        elif activation_function == 'relu': self.act = Relu()  # activation function
+
+        # initialize with random weights
+        self.randomize_weights()
+
+    def randomize_weights(self):
+
+        for layer in range(self.layers):
+
+            # end before adding weights to output layer
+            if layer == self.layers-1:
+                break
+            else:
+                # Number of nodes in previous layer determines random weight initial values
+                r = 1 / math.sqrt(self.layout[layer])
+
+                # Create weight matrix array
+                self.weights.append([])
+                for node in range(self.layout[layer+1]):
+                    self.weights[-1].append([])
+                    for neuron in range(self.layout[layer]):
+                        self.weights[-1][-1].append(random.uniform(-r, r))
+
+                    # add weight for bias neuron if present
+                    if self.__bias:
+                        self.weights[-1][-1].append(random.uniform(-r, r))
+
+    def forward(self, inputs_a, debug=False):
+        inputs = copy.deepcopy(inputs_a)
+        # Check arguments for bad input
+        if type(inputs) != list:
+            raise TypeError("expected inputs argument as type list, got {}".format(type(inputs)))
+
+        elif len(inputs[0]) != self.layout[0]:
+            raise IndexError("Incorrect number of inputs for network, expected {} got {}".format(self.layout[0],
+                                                                                                 len(inputs[0])))
+
+        if debug: print("#---------------------------FORWARD-------------------------------#\n")
+
+        self.layer_sums = []    # Clean list for new sums
+        self.layer_inputs = []  # Clean list for new inputs
+
+        # Run inputs through entire network ending on the output layer which will return the result.
+        for layer in range(self.layers-1):
+
+            if debug: print("#-------------------Layer({})-----------------------#".format(layer))
+
+            if debug: print("Inputs: ", inputs)
+            if debug: print("Weights", self.weights[layer])
+
+            if self.__bias:
+                inputs[0].append(1.0)
+
+            self.layer_inputs.append([n for n in inputs[0]])
+
+            self.layer_sums.append(multiply_2d(inputs, transpose_2d(self.weights[layer]))[0])
+
+            outputs = [[self.act.activate(col) for col in self.layer_sums[layer]]]
+
+            if debug: print("Layer sums: ", self.layer_sums[layer])
+
+
+            # use output as input for next layer
+            inputs = outputs
+
+        # return the output of the last layer calculated
+        if debug: print("Output of forward propagation: ", outputs)
+
+        return outputs
+
+    def backward(self, error, debug=False):
+        if debug: print("#-----------------------------BACKWARD-------------------------------#\n")
+        layer = self.layers-2  # Start on the first hidden layer before output neurons
+
+        # setup up the derivative of layer sums for delta calculation below
+        self.layer_sums = [[self.act.derivative(val) for val in layer] for layer in self.layer_sums]
+
+        while layer >= 0:
+            if debug: print("#-------------layer{}---------------#".format(layer))
+            if debug: print("Layer sums: ", self.layer_sums[layer])
+
+            # Multiply error by sum to get change needed in nodes
+
+            delta = [[a*b for a,b in zip(self.layer_sums[layer], error)]]
+
+            if debug: print("Delta (Change needed in each node):", delta)
+
+            # Get weights for actual neurons not including bias
+            if self.__bias:
+                layer_weights = []
+                for neuron in self.weights[layer]:
+                    layer_weights.append(neuron[:-1])
+            else:
+                layer_weights = self.weights[layer]
+
+            if debug: print("Weights to previous layer: ", self.weights[layer])
+
+            # Transmit error over weights to previous layer
+            output = multiply_2d(delta, layer_weights)
+
+            if debug: print("Error transmitted to previous layer: ", output)
+
+            # set error for next layer
+            error = output[0]
+
+            t = multiply_constant(self.learning_constant, multiply_2d(transpose_2d(delta), [self.layer_inputs[layer]]))
+
+            # Adjust weights for this layer
+            self.weights[layer] = add_2d(t, self.weights[layer])
+
+            if debug: print("Adjusted weights: ", self.weights[layer])
+
+            layer -= 1
+
+    def train(self, inputs, target, debug=False):
+        """
+        Runs one step of forward and backward propagation given appropriate data sets. Returns results and error after
+        forward propagation and before adjusting network weights to reduce error.
+
+        :param inputs: (list)   Matrix (list of lists) of inputs for the network, one value for each input in network.
+        :param target: (list)   Matrix (list of lists) of target output values, one value for each output in network.
+        :return: (list, list)   Matrices containing the result of forward propagation and the calculated network error
+        """
+        result = self.forward(inputs, debug=debug)
+        e = subtract_2d(target, result)
+        self.backward(e[0], debug=debug)
+
+        return result, e
+
+    def set_bias(self, value):
+        """
+        Method of activating or deactivating the bias inputs in the network layers. Removes associated weights when
+        deactivated. When (re)activated bias weights are assigned a random value between -0.2 and 0.2 and do not use
+        any old value that might have been used before deactivation.
+
+        :param value: (bool)  True: activate bias, False: deactivate bias.
+        :return: None
+        """
+        if type(value) == bool:
+            if self.__bias == value:
+                return
+
+            # If disabling bias remove corresponding weights
+            elif self.__bias and not value:
+                for layer in self.weights:
+                    for neuron in layer:
+                        del neuron[-1]
+
+            # If activating bias add corresponding weights
+            elif value and not self.__bias:
+                for layer in self.weights:
+                    for neuron in layer:
+                        neuron.append(random.uniform(-0.2, 0.2))
+
+            self.__bias = value
+
+        else:
+            raise TypeError("Expected type bool, got {}".format(type(value)))
+
+    def set_weights(self, weight_array):
+        """
+        Method of setting user defined weights for the network. Mostly useful for testing or debugging.
+
+        :param weight_array:    List, formatted as a list of lists of lists representing the overall network, layers,
+        and neuron weights, respectively.
+
+        :return: N/A
+        """
+        if type(weight_array) != list:
+            raise TypeError("Expected list, got {}".format(type(weight_array)))
+        else:
+            self.weights = weight_array
+
+    def get_bias(self):
+        return self.__bias
+
+
+
